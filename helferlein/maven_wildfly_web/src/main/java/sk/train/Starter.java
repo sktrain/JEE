7fe940880f602aa8802a715a640f2b8e0cd7c94d
sktrain <39360116+sktrain@users.noreply.github.com>
2024-02-01 08:37:38 +0100

    Add files via upload

---
+++ helferlein/maven_wildfly_web/src/main/java/sk/train/Starter.java
@@ -0,0 +1,36 @@
+package sk.train;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+/**
+ * Servlet implementation class Starter
+ */
+@WebServlet("/Starter")
+public class Starter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Starter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath()).append('\n');
+	}
+
+}
