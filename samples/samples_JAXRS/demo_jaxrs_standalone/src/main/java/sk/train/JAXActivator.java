8420875ebba20298eead7c5a0bc6ea8abc968733
sktrain <39360116+sktrain@users.noreply.github.com>
2024-07-11 13:06:14 +0200

    Add files via upload

---
+++ samples/samples_JAXRS/demo_jaxrs_standalone/src/main/java/sk/train/JAXActivator.java
@@ -0,0 +1,10 @@
+package sk.train;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
+@ApplicationPath("rest")
+public class JAXActivator extends Application {
+    // Left empty intentionally
+}
