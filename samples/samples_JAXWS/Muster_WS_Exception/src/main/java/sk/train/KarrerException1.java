2ba950300e7c43b7908ab43a467d5e3212dbd867
sktrain <39360116+sktrain@users.noreply.github.com>
2025-08-03 23:08:46 +0200

    Delete samples/samples_JAXWS directory
    ersetzen gegen aktuelle Versionen

--- samples/samples_JAXWS/Muster_WS_Exception/src/main/java/sk/train/KarrerException1.java
+++
@@ -1,9 +0,0 @@
-package sk.train;
-
-public class KarrerException1 extends Exception {
-	
-	public KarrerException1(String reason) {
-		super(reason);		
-	}
-	
-}
--- samples/samples_JAXWS/Muster_WS_Hello_NoInterface_Client/src/main/java/sk/train/gen/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://train.sk/")
-package sk.train.gen;
